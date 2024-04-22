4ff9109451e6eb78fef8e570ff3db31ef7e88307
akinyemihassan <akinyemitaiwo646@gmail.com>
2024-04-22 15:29:33 -0700

    Added new feature

--- src/App.js
+++ src/App.js
@@ -25,26 +25,54 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+
+  const [curOpen, setCurOpen]=useState(null)
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.text} />
+        <AccordionItem
+         curOpen={curOpen} 
+         onOpen={setCurOpen}
+         title={el.title}   
+          num={i}
+           key={el.text} 
+           >{el.text}</AccordionItem>
+       
       ))}
+      <AccordionItem
+         curOpen={curOpen} 
+         onOpen={setCurOpen}
+         title="Think React! If you desire reusablity"  
+          num={22}
+           key="New reusable piece"
+           >
+            <p>Allows react developers to:</p>
+            <ul>
+              <li>Break Up UI into component</li>
+              <li>Make components reusable</li>
+              <li>Place state efficiently</li>
+            </ul>
+           </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen,children }) {
+  // const [isOpen, setIsOpen] = useState(false);
+  const isOpen=num === curOpen;
+
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    // setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num)
+
   }
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
